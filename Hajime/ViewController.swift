a86649059272cabb633c2bb6674c5e566df832f8
Robin <dev@roeegh.com>
2023-08-22 23:12:35 -0500

    Initial Commit

---
+++ Hajime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hajime
+//
+//  Created by Robin 🌸 on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
