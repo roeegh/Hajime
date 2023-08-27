9645b6e924236be4774a5330e81a899d36df1e0e
Robin <dev@roeegh.com>
2023-08-27 18:26:51 -0500

    🌸 Initial Commit

--- Hajime/ViewController.swift
+++ Hajime/ViewController.swift
@@ -6,14 +6,86 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reloadBackground()
+        
+        // Handle PFP
+        PFP.layer.borderWidth = 5
+        PFP.layer.cornerRadius = PFP.frame.width / 2
+        PFP.clipsToBounds = true
+        
+        // Handle Twitter
+        let twitterGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(openTwitter))
+        Twitter.addGestureRecognizer(twitterGestureRecognizer)
+        
+        // Handle Github
+        let githubGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(openGithub))
+        Github.addGestureRecognizer(githubGestureRecognizer)
+        
+        // Handle Reload
+        let reloadGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(reloadBackground))
+        Reload.addGestureRecognizer(reloadGestureRecognizer)
+        
+        // Handle Map
+        let utdCoordinates = CLLocationCoordinate2D(latitude: 32.986633, longitude: -96.750508)
+        let camera = MKMapCamera(lookingAtCenter: utdCoordinates, fromDistance: 500, pitch: 45, heading: 0)
+
+        Map.setCamera(camera, animated: false)
+        Map.isUserInteractionEnabled = false
+    }
+    
+    // Runs on Twitter Tap
+    @objc func openTwitter() {
+        if let url = URL(string: "https://twitter.com/roeegh") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    // Runs on Github Tap
+    @objc func openGithub() {
+        if let url = URL(string: "https://github.com/roeegh") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+    // Runs on Reload Tap
+    @objc func reloadBackground() {
+        let mainBackground = generateRandomColor()
+        let secondaryBackground = UIColor { traits in
+            return traits.userInterfaceStyle == .dark ? UIColor(red: 0, green: 0, blue: 0, alpha: 0.50) : UIColor(red: 1, green: 1, blue: 1, alpha: 0.50)
+        }
+        
+        view.backgroundColor = mainBackground
+        PFP.layer.borderColor = mainBackground.cgColor
+        Bio.textColor = mainBackground
+        
+        Profile.backgroundColor = secondaryBackground
+        Map.backgroundColor = secondaryBackground
+        Twitter.backgroundColor = secondaryBackground
+        Github.backgroundColor = secondaryBackground
+        Reload.backgroundColor = secondaryBackground
+    }
+    
+    // Generates random color
+    func generateRandomColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 
+    @IBOutlet weak var Profile: UIView!
+    @IBOutlet weak var PFP: UIImageView!
+    @IBOutlet weak var Bio: UILabel!
+    @IBOutlet weak var Map: MKMapView!
+    @IBOutlet weak var Twitter: UIView!
+    @IBOutlet weak var Github: UIView!
+    @IBOutlet weak var Reload: UIView!
 }
-
---
+++ README.md
@@ -0,0 +1,54 @@
+# Prework - *Hajime*
+
+Submitted by: **Randeep Singh**
+
+**Hajime** is an app that provides a self introduction for myself. It also changes the background color of the app when a user taps a button. It also has buttons that link to my Twitter and Github profiles.
+
+Time spent: **8** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] Users are see a screen with three labels and a button
+- [x] Tapping the button changes the screen color to a random color
+ 
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://i.imgur.com/YhcHSQJ.mp4' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [Gifski](https://sindresorhus.com/gifski)
+
+## App Brainstorming (Step 4)
+
+A few of my favorite applications are:
+- Sileo
+- Snapchat
+- App Store
+
+Some of the features I like about these apps are:
+- Sileo: I like how it works as a repo system manager and also shows the depictions of different tweaks.
+- Snapchat: I like how it has a lot of filters and how it does a lot of image processing.
+- App Store: I like the simple design and the animations present on the Today page.
+
+## Notes
+
+The hardest part of building my app was understanding how constraints worked and making sure the size of everything looked the way I wanted it to.
+
+## License
+
+    Copyright 2023 Robin
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
